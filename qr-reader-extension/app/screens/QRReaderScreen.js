df6e9d6feedafbc335e9b149f7e82ef5d10d2f17
Vladimir Vdovic <vladimir.vdovic@shoutem.com>
2017-05-22 15:25:24 +0200

    Add example for native 3rd party package installation

---
+++ qr-reader-extension/app/screens/QRReaderScreen.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { Alert } from 'react-native';
+import Camera from 'react-native-camera';
+import _ from 'lodash';
+
+export default class QRReaderScreen extends Component {
+    constructor(props) {
+      super(props);
+      this.onBarCodeRead = this.onBarCodeRead.bind(this);
+    }
+
+  onBarCodeRead(code) {
+      Alert.alert(
+        'QR Code Detected',
+        code.data,
+        [
+          {text: 'OK, read it.', onPress: () => console.log('User saw QR Code contents.')},
+        ],
+        { cancelable: false }
+      )
+  }
+
+  render() {
+    return (
+      <Camera
+        onBarCodeRead={_.debounce(this.onBarCodeRead, 1000, { leading: true, trailing: false })}
+        style={{ flex: 1 }}
+      />
+    );
+  }
+}
---
+++ qr-reader-extension/app/scripts/run.js
@@ -0,0 +1,21 @@
+const fs = require('fs-extra');
+const plist = require('plist');
+
+const infoPlistPath = './ios/ShoutemApp/Info.plist';
+const infoPlistFile = fs.readFileSync(infoPlistPath, 'utf8');
+const infoPlist = plist.parse(infoPlistFile);
+
+console.log('Adding camera and microphone permissions to Info.plist');
+infoPlist['NSCameraUsageDescription'] = 'App needs your camera to be able to scan QR codes';
+infoPlist['NSMicrophoneUsageDescription'] = 'App needs your microphone to be able to scan QR codes';
+fs.writeFileSync(infoPlistPath, plist.build(infoPlist));
+
+const exec = require('child_process').execSync;
+
+const dependenciesToLink = ['react-native-camera'];
+
+const command = 'node node_modules/react-native/local-cli/cli.js link';
+
+dependenciesToLink.forEach((dependency) => {
+  exec(`${command} ${dependency}`);
+});
